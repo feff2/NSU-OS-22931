0d969914c988a822fa9efee433c56d8a7a385f4d
pekpuch <114422891+pekpuch@users.noreply.github.com>
2023-12-22 01:17:36 +0700

    Правка server.c lab31

--- n.glazkov/lab 30/server.c
+++ n.glazkov/lab 30/server.c
@@ -9,12 +9,13 @@
 #define SOCKET_PATH "/tmp/socket"
 
 int main() {
-    int client_fd;
-    struct sockaddr_un server_addr;
+    int server_fd, client_fd;
+    struct sockaddr_un server_addr, client_addr;
+    socklen_t client_len;
+    char buf[1025];
 
     // Создание сокета
-    client_fd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (client_fd == -1) {
+    if ((server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
@@ -24,24 +25,43 @@ int main() {
     server_addr.sun_family = AF_UNIX;
     strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
 
-    // Установка соединения с сервером
-    if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) == -1) {
-        perror("connect");
+    // Привязка сокета к адресу
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_un)) == -1) {
+        perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    // Отправка текста на сервер
-    char message[1024];
+    // Прослушивание сокета
+    if (listen(server_fd, 5) == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
 
-    printf("Enter text: ");
-    fgets(message, 1024, stdin);
-    send(client_fd, message, strlen(message), 0);
+    // Принятие соединения от клиента
+    client_len = sizeof(struct sockaddr_un);
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len)) == -1) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
 
-    perror("send");	
-    exit(EXIT_FAILURE);
+    // Чтение данных от клиента
+    if (read(client_fd, buf, sizeof(buf)) == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
 
-    // Закрытие сокета
+    // Преобразование текста в верхний регистр
+    for (int i = 0; i < strlen(buf); i++) {
+        buf[i] = toupper(buf[i]);
+    }
+
+    // Вывод преобразованного текста
+    printf("Received from client: %s\n", buf);
+
+    // Закрытие сокетов
     close(client_fd);
+    close(server_fd);
+    unlink(SOCKET_PATH);
 
     return 0;
 }
